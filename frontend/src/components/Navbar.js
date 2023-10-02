61390718d168c493e82a54247a6df65f6b6d4aa4
Abukar <abukaraweis97@gmail.com>
2023-10-02 16:58:32 -0500

    backend & frontend initial set up

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    
+    return (
+        <header>
+            <div className="container">
+                <h1>Workout Tracker</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
